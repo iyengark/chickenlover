72d668cf365e81ae90ca13dcc157b942aaf523c0
iyengark <krishnan.iyengar@outlook.com>
2016-08-03 13:43:35 -0400

    comments.json as of {AUG-03-2016}

--- nytcl.sh
+++ nytcl.sh
@@ -1,7 +1,7 @@
 cd /Users/krishnan/sandbox/chickenlover
 [ -f comments.json.bkup ] && rm comments.json.bkup
 [ -f comments.json ] && mv comments.json comments.json.bkup
-echo MMM-DD-YYYY
+echo MMM-DD-YYYY $1
 git status
 git pull
 python nytapi.py && git add comments.json && git commit -a -m "comments.json as of {$1}" && git push origin gh-pages
